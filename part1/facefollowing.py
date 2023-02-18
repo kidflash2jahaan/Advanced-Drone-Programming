7e7c6e07bec70dfb93022f88b6a951007e4b0be5
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2023-02-18 09:05:34 -0800

    facefollowing.py: Prepare for Flight

--- part1/facefollowing.py
+++ part1/facefollowing.py
@@ -3,29 +3,30 @@ import cv2
 import cvzone
 from cvzone.FaceDetectionModule import FaceDetector
 
-cap = cv2.VideoCapture(0)
-detector = FaceDetector(minDetectionCon=0.8)
+detector = FaceDetector(minDetectionCon=0.5)
 
-_, img = cap.read()
-hi, wi, _ = img.shape
+# cap = cv2.VideoCapture(0)
+# _, img = cap.read()
 
-xPID = cvzone.PID([0.3, 0, 0.1], wi // 2)
-yPID = cvzone.PID([0.3, 0, 0.1], hi // 2, axis=1)
-zPID = cvzone.PID([0.003, 0, 0.1], 30000)
+hi, wi, _ = 640, 480
+
+xPID = cvzone.PID([0.3, 0, 0], wi // 2)
+yPID = cvzone.PID([0.3, 0, 0], hi // 2, axis=1)
+zPID = cvzone.PID([0.003, 0, 0], 30000)
 
 myPlotX = cvzone.LivePlot(yLimit=[-100, 100], char='X')
 myPlotY = cvzone.LivePlot(yLimit=[-100, 100], char='Y')
 myPlotZ = cvzone.LivePlot(yLimit=[-100, 100], char='Z')
 
-# me = tello.Tello()
-# me.connect()
-# print(me.get_battery())
-# me.streamoff()
-# me.streamon()
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+me.streamoff()
+me.streamon()
 
 while True:
-    _, img = cap.read()
-    # img = me.get_frame_read().frame
+    # _, img = cap.read()
+    img = me.get_frame_read().frame
     img, bboxs = detector.findFaces(img)
 
     if bboxs:
@@ -45,18 +46,11 @@ while True:
         img = xPID.draw(img, [cx, cy])
         img = yPID.draw(img, [cx, cy])
 
-        # cv2.putText(img, str(xVal), (50, 100),
-        # cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        # cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
-        # cv2.line(img, (wi // 2, 0), (wi // 2, hi), (255, 0, 255), 1)
-        # cv2.line(img, (wi // 2, hi // 2), (cx, cy), (255, 0, 255), 3)
-        # error = wi // 2 - cx
-        # cv2.putText(img, str(error), (50, 100),
-        #             cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
     imgStacked = cvzone.stackImages(
         [img, imgPlotX, imgPlotY, imgPlotZ], 2, 0.75)
 
+    # me.send_rc_control(0, zVal, yVal, xVal)
+    me.send_rc_control(0, 0, 0, xVal)
     cv2.imshow('Image', imgStacked)
 
     if cv2.waitKey(5) & 0xFF == ord('q'):
